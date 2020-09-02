0d4dcdf6c274585866eb98e48f251cca3967ed33
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2020-09-02 13:58:53 +0900

    eslintの設定追加

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: ['plugin:prettier/recommended', 'plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
+  parser: 'babel-eslint',
+  plugins: ['react'],
+  rules: {
+    'global-require': 0,
+    'no-console': 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/prop-types': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 120,
+        singleQuote: true,
+        trailingComma: 'all',
+      },
+    ],
+  },
+};
