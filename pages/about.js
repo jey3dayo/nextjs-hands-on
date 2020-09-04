e46a90fa80b6a761027718757cac897d7fb10cab
Junya Nakazato <nakazato_junya@ca-adv.co.jp>
2020-09-04 15:32:04 +0900

    lint

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Link from 'next/link';
+import Button from '@material-ui/core/Button';
 
 const About = () => {
   return (
     <>
       <div>about page</div>
       <Link href="/">Home</Link>
+      <br />
+      <Button variant="contained" color="primary">
+        Hello!
+      </Button>
     </>
   );
 };
